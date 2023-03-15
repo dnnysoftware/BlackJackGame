5a0c6b251200e767c255d03365aa63749898eb0e
Daniel Mironiuk <softwarebydanielmironiuk@gmail.com>
2023-03-15 18:58:07 -0400

    Sorted deck of cards

--- deck/card_test.go
+++ deck/card_test.go
@@ -1,10 +1,30 @@
 package deck
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func ExampleCard() {
 	fmt.Println(Card{Rank: Ace, Suit: Heart})
+	fmt.Println(Card{Suit: Joker})
 
 	// Output:
 	// Ace of Hearts
+	// Joker
+}
+
+func TestNew(t *testing.T) {
+	cards := New()
+	if len(cards) != 13*4 {
+		t.Error("Wrong number of cards in the deck")
+	}
+}
+
+func TestDefaultSortNew(t *testing.T) {
+	cards := New(DefaultSort)
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Wrong number of cards in the deck")
+	}
 }
