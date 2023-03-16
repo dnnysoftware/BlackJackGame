d257446f21dc845e0140e99b8b629a24cc1a0dba
Daniel Mironiuk <softwarebydanielmironiuk@gmail.com>
2023-03-15 20:03:50 -0400

    Got basic dealer functionality completed, now just need to add game states

--- main.go
+++ main.go
@@ -24,6 +24,34 @@ func (h Hand) DealerString() string {
 	return h[0].String() + ", *HIDDEN*"
 }
 
+func (h Hand) MinScore() int {
+	score := 0
+	for _, card := range h {
+		score += min(int(card.Rank), 10)
+	}
+	return score
+}
+
+func (h Hand) Score() int {
+	minScore := h.MinScore()
+	if minScore > 11 {
+		return minScore
+	}
+	for _, card := range h {
+		if card.Rank == deck.Ace {
+			return minScore + 10
+		}
+	}
+	return minScore
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func main() {
 	cards := deck.New(deck.Deck(6), deck.Shuffle)
 	var card deck.Card
@@ -46,7 +74,24 @@ func main() {
 			player = append(player, card)
 		}
 	}
+	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
+		card, cards = draw(cards)
+		dealer = append(dealer, card)
+	}
+	pScore, dScore := player.Score(), dealer.Score()
 	fmt.Println("--- Final Hands ---")
-	fmt.Println("Player:", player)
-	fmt.Println("Dealer:", dealer)
+	fmt.Println("Player:", player, "\nScore:", pScore)
+	fmt.Println("Dealer:", dealer, "\nScore:", dScore)
+	switch {
+	case pScore > 21:
+		fmt.Println("You Busted, You Lose")
+	case dScore > 21:
+		fmt.Println("Dealer Busted, You Win")
+	case pScore > dScore:
+		fmt.Println("You Win")
+	case pScore < dScore:
+		fmt.Println("You Lose")
+	case pScore == dScore:
+		fmt.Println("You Draw With The Dealer")
+	}
 }
